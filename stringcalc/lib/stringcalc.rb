ea03c0fc07fa49327a651f3837dc41fad6c50b21
Maharajan <maharajaskm@gmail.com>
2025-05-21 19:38:53 +0530

    Ror assessment string calculator code

--- stringcalc/lib/stringcalc.rb
+++ stringcalc/lib/stringcalc.rb
@@ -2,6 +2,7 @@ class Stringcalc
   def self.add(num)
     sum_of_all = 0
     return sum_of_all if num == ''
+    num.gsub!(/[!@%&"#\\n$^*?><;:]/,',')
     num.split('').each do |n|
         sum_of_all += n.to_i
       end
--- stringcalc/spec/stringcalc_spec.rb
+++ stringcalc/spec/stringcalc_spec.rb
@@ -16,5 +16,138 @@ describe Stringcalc do
       end
     end
   end
+  describe ".add" do
+    context "given multiple inputs with comma seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1,2,3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs begins with newline seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("\n1,2,3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with intermediate newline seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1,2\n3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with tail newline seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1,2,3\n")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs begins with # seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("#1,2,3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with intermediate # seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1#2,3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with tail # seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("#1,2,3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs begins with ! seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("!1,2,3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with intermediate ! seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1!2,3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with tail ! seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1,2,3!")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs begins with @ seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("@1,2,3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with intermediate @ seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1@2,3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with tail @ seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1,2,3@")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs begins with $ seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("$1,2,3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with intermediate $ seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1,2$3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with tail $ seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1,2,3$")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs begins with % seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("%1,2,3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with intermediate % seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1,2%3")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given multiple inputs with tail % seperater" do
+      it "returns sum of all" do
+        expect(Stringcalc.add("1,2,3%")).to eq(6)
+      end
+    end
+  end
 
 end
