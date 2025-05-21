c4ae5736a58955605b3ed4e077f958eaf4488091
Maharajan <maharajaskm@gmail.com>
2025-05-21 20:26:55 +0530

    Ror assessment string calculator code

--- stringcalc/lib/stringcalc.rb
+++ stringcalc/lib/stringcalc.rb
@@ -2,10 +2,11 @@ class Stringcalc
   def self.add(num)
     sum_of_all = 0
     return sum_of_all if num == ''
-    num.gsub!(/[!@%&"#\\n$^*?><;:]/,',')
-    num.split('').each do |n|
+    num.gsub!(/[!@%&"#\n$^*?><;:]/,',')
+    num.split(',').each do |n|
+      raise "Negative numbers not allowed #{n}" if n.to_i < 0
         sum_of_all += n.to_i
-      end
-      return sum_of_all
+    end
+    return sum_of_all
   end
 end
--- stringcalc/spec/stringcalc_spec.rb
+++ stringcalc/spec/stringcalc_spec.rb
@@ -1,7 +1,7 @@
 require './lib/stringcalc'
 
 describe Stringcalc do
-
+  
   describe ".add" do
     context "given an empty string" do
       it "returns zero" do
@@ -19,7 +19,7 @@ describe Stringcalc do
   describe ".add" do
     context "given multiple inputs with comma seperater" do
       it "returns sum of all" do
-        expect(Stringcalc.add("1,2,3")).to eq(6)
+        expect(Stringcalc.add("1,2,30")).to eq(33)
       end
     end
   end
@@ -314,6 +314,34 @@ describe Stringcalc do
     context "given multiple inputs with tail ? seperater" do
       it "returns sum of all" do
         expect(Stringcalc.add("1,2,3?")).to eq(6)
+      end
+    end
+  end
+  describe ".add" do
+    context "given a negative number" do
+      it "returns exception" do
+        expect{Stringcalc.add("-1")}.to raise_error('Negative numbers not allowed -1')
+      end
+    end
+  end
+  describe ".add" do
+    context "given a intermediate negative number" do
+      it "returns exception" do
+        expect{Stringcalc.add("1,-2,3")}.to raise_error('Negative numbers not allowed -2')
+      end
+    end
+  end
+  describe ".add" do
+    context "given a tail negative number" do
+      it "returns exception" do
+        expect{Stringcalc.add("1,2,-3")}.to raise_error('Negative numbers not allowed -3')
+      end
+    end
+  end
+  describe ".add" do
+    context "given a random numbers with special characters" do
+      it "returns exception" do
+        expect{Stringcalc.add("1#2,-3")}.to raise_error('Negative numbers not allowed -3')
       end
     end
   end
